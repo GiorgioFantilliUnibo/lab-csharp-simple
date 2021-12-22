7b522e557d7acee8c0d6506144e41c84045925e5
Giorgio Fantilli <giorgio.fantilli@studio.unibo.it>
2021-12-23 00:09:07 +0100

    fix: correct wrong order in test array

--- 4-Arrays/Program.cs
+++ 4-Arrays/Program.cs
@@ -97,28 +97,28 @@ namespace Arrays
         static void Main(string[] args)
         {
             Complex[] numbers = new[] {
-                 new Complex(0, 0),
-                 new Complex(1, 1),
-                 new Complex(0, 1),
-                 new Complex(-2, 2),
-                 new Complex(-3, 0),
-                 new Complex(-2, -2),
-                 new Complex(0, -4),
-                 new Complex(1, -1),
-                 new Complex(1, 0)
-             };
+                new Complex(0, 0),
+                new Complex(1, 1),
+                new Complex(0, 1),
+                new Complex(-2, 2),
+                new Complex(-3, 0),
+                new Complex(-2, -2),
+                new Complex(0, -4),
+                new Complex(1, -1),
+                new Complex(1, 0)
+            };
 
             Complex[] orderedByPhase = new[] {
-                 new Complex(0, 0),
-                 new Complex(1, 1),
-                 new Complex(0, 1),
-                 new Complex(-2, 2),
-                 new Complex(-3, 0),
-                 new Complex(-2, -2),
-                 new Complex(0, -4),
-                 new Complex(1, -1),
-                 new Complex(1, 0),
-             };
+                new Complex(-2, -2),
+                new Complex(0, -4),
+                new Complex(1, -1),
+                new Complex(0, 0),
+                new Complex(1, 0),
+                new Complex(1, 1),
+                new Complex(0, 1),
+                new Complex(-2, 2),
+                new Complex(-3, 0)
+            };
 
             var cloned = numbers;
 
